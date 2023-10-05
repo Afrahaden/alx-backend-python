cd7342a7fd9ab98deac3811150714289c5bdfe8e
Afrah Aden <afrahaden05@gmail.com>
2023-10-05 17:07:29 +0000

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3#!
 """
     Module with typing Python
+"""
+def add(a: float, b: float) -> float:
     """
-
-
-    def add(a: float, b: float) -> float:
-            """
-                    Args:
-                                a: The first parameter
-                                            b: The second parameter
-
-                                                    Return:
-                                                                Sum of two floats
-
-                                                                    """
-
-                                                                        return (a + b)
+     Args:
+      a: The first parameter
+      b: The second parameter
+      Return:
+        Sum of two floats
+    """
+    return (a + b)
